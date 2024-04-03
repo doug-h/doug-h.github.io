5ab66fc993ad168364a1954897b648c0083fbae0
doug-h <hull.douglasj@gmail.com>
2024-04-03 14:06:08 +0100

    Minor Solitaire formatting

--- posts/solitaire/src/build.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p -- "build"
-
-cxxflags="-std=c++23 -sINITIAL_MEMORY=256KB -sUSE_SDL=2 -sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2 -sGL_TESTING"
-warnings="-Wall -Wpedantic -Wsign-conversion -Wno-gnu-anonymous-struct -Wno-nested-anon-types"
-debugflags="-sSAFE_HEAP=1 -sSTACK_OVERFLOW_CHECK=2 -fno-omit-frame-pointer -g"
-releaseflags="-DNDEBUG -O3"
-
-# em++ src/main.cpp -o build/main.js %cxxflags% %debugflags% %warnings%
-em++ main.cpp -o ../solitaire.js $cxxflags $releaseflags
